b2deef0d31e4410d4df84e5423703cc26b1fabeb
Mister Frank <130546248+misterfrankw@users.noreply.github.com>
2025-03-20 23:29:39 +0200

    Update cursor-update.sh

--- cursor-update.sh
+++ cursor-update.sh
@@ -2,7 +2,7 @@
 
 BINDIR=$HOME/bin
 TEMPDIR=/tmp/cursor
-APPIMAGE_URL="https://downloader.cursor.sh/linux/appImage/x64"
+APPIMAGE_URL="https://downloads.cursor.com/production/client/linux/x64/appimage/Cursor-0.47.8-82ef0f61c01d079d1b7e5ab04d88499d5af500e3.deb.glibc2.25-x86_64.AppImage"
 
 mkdir -p $TEMPDIR
 pushd $TEMPDIR
